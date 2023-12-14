ed4ec79995f78f94ae5ef841e620766c10052581
Steven Chen <stevencn76@gmail.com>
2023-12-13 17:16:18 -0800

    hash password

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import bcrypt
 from flask_login import UserMixin
 
 from routes import db, login_manager
@@ -16,10 +17,11 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String, nullable=False)
-    fullname: Mapped[str] = mapped_column(String, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=True)
+    username: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    fullname: Mapped[str] = mapped_column(String(128), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
 
     def check_password_correction(self, attempted_password):
-        return self.password == attempted_password
+        password_hashed = self.password.encode()
+        return bcrypt.checkpw(attempted_password.encode(), password_hashed)
